c312fbee9c5c7a84812354797bb5c10f293e83c7
june0326 <6ixthmonth@gmail.com>
2022-05-26 16:22:26 +0900

    Remove unnecessary files

--- JDBC & MyBatis/Examples/MyBatis 게시판 예제 코드/SCITCommunity/src/dao/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import vo.User;
-
-public interface UserMapper {
-	public int signup(User usr);
-	public String login(User usr);
-}
--- Java/Examples/Java 예제 코드/TestProject/src/thread/CakeTest.java
+++
@@ -1,9 +0,0 @@
-package thread;
-
-public class CakeTest {
-	public static void main(String[] args) {
-		Cake cake = new Cake();
-		new Thread(new Producer(cake)).start();
-		new Thread(new Consumer(cake)).start();
-	}
-}
